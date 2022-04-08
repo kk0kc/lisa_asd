0343cf0c0e1bcb7d4383469fcd95a2defb6195f7
kk0kc <lizagoldova13@gmail.com>
2022-04-09 02:53:37 +0300

    update semester

--- FenwickTree/src/FenwickTree.java
+++ FenwickTree/src/FenwickTree.java
@@ -11,6 +11,7 @@ public class FenwickTree {
         this(initValues.size());
         for (int i = 0; i < initValues.size(); i++) {
             setValue(i, initValues.get(i));
+
         }
     }
 
@@ -21,49 +22,39 @@ public class FenwickTree {
     }
 
     public void setValue(int index, int value) {
-        long oldValue = get(index);
-        long difference = value - oldValue;
-        while (index < btree.length) {
+        long difference = value - get(index);
+        for (; index < btree.length; index = (index | (index+1)))
             btree[index] += difference;
-            index |= index + 1;
-        }
     }
 
-    public long getSum(int fromIndexInclusive, int toIndexInclusive) {
-        if (fromIndexInclusive > toIndexInclusive) {
-            throw new IllegalArgumentException("from index more than to index");
-        }
-        long leftSum = fromIndexInclusive > 0 ? getSum(fromIndexInclusive - 1) : 0;
-        return getSum(toIndexInclusive) - leftSum;
+    public long getSum(int from, int to) {
+        return getSum(to) - getSum(from - 1);
     }
 
-    public long getSum(int toIndexInclusive) {
-        if (toIndexInclusive >= btree.length) {
-            throw new IllegalArgumentException("to index more than btree length");
-        }
+    public long getSum(int to) {
         long result = 0;
-        while (toIndexInclusive >= 0) {
-            result += btree[toIndexInclusive];
-            if (toIndexInclusive == 0) {
+        while (to >= 0) {
+            result += btree[to];
+            if (to == 0) {
                 break;
             }
-            toIndexInclusive &= toIndexInclusive + 1;
-            toIndexInclusive--;
+            to &= to + 1;
+            to--;
         }
         return result;
     }
     public String toString(){
         return Arrays.toString(Arrays.copyOf(btree, btree.length));
     }
-//    public static void main(String[] args){
-//        ArrayList val =  new ArrayList(Arrays.asList(1,3,4,5,6));
-//        System.out.println(val);
-//        int n = val.size();
-//        FenwickTree btree = new FenwickTree(val);
-//        System.out.println(btree);
-//        System.out.println(btree.getSum(2,4));
-//        btree.setValue(3, 10);
-//        System.out.println(btree.getSum(2,4));
-//        System.out.println(btree);
-//    }
+    public static void main(String[] args){
+        ArrayList val =  new ArrayList(Arrays.asList(1,3,4,5,6));
+        System.out.println(val);
+        int n = val.size();
+        FenwickTree btree = new FenwickTree(val);
+        System.out.println(btree);
+        System.out.println(btree.getSum(2,4));
+        btree.setValue(3, 10);
+        System.out.println(btree.getSum(2,4));
+        System.out.println(btree);
+    }
 }
--- FenwickTree/src/Test.java
+++ FenwickTree/src/Test.java
@@ -11,7 +11,7 @@ public class Test {
         for (int n = 1; n < 101; n++) {
             t1 = 0;
             for (int k = 0; k < 25; k++) {
-                long start1 = System.currentTimeMillis();
+                long start1 = System.nanoTime();
                 FileReader file = new FileReader("FenwickTree/FilesForTest/FILE " + n + ".txt");
                 Scanner scanner = new Scanner(file);
                 ArrayList<Integer> listValues = new ArrayList<>();
@@ -26,18 +26,19 @@ public class Test {
                 int to = listValues.size()-2;
                 tree.getSum(from, to);
                 file.close();
+                long finish1 = System.nanoTime();
 
-                long finish1 = System.currentTimeMillis();
+
                 t1 += (finish1 - start1);
             }
-                System.out.println(t1/25 );
+            System.out.println(t1/25 );
         }
         System.out.println("2 test");
         for (int n = 1; n < 101; n++) {
             t2 = 0;
             for (int k = 0; k < 25; k++) {
-                long start1 = System.currentTimeMillis();
 
+                long start1 = System.nanoTime();
                 FileReader file = new FileReader("FenwickTree/FilesForTest/FILE " + n + ".txt");
                 Scanner scanner = new Scanner(file);
                 ArrayList<Integer> listValues = new ArrayList<>();
@@ -52,8 +53,9 @@ public class Test {
                 int val = 43593;
                 tree.setValue(index, val);
                 file.close();
+                long finish1 = System.nanoTime();
 
-                long finish1 = System.currentTimeMillis();
+
                 t2 += (finish1 - start1);
             }
             System.out.println(t2/25 );
